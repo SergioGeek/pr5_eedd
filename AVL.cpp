6fda0df9178900e6364a328ac872f736d7e987f6
SergioGeek <sergio.it.geek@gmail.com>
2017-11-13 01:23:09 +0100

    creamos la clase AVL y sus constructores

---
+++ AVL.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anonymous on 12/11/17.
+//
+
+#include "AVL.h"
---
+++ AVL.h
@@ -0,0 +1,62 @@
+
+#ifndef PR5_EEDD_AVL_H
+#define PR5_EEDD_AVL_H
+
+template <typename T>
+class Nodo
+{
+public:
+    Nodo<T> *izq;
+    Nodo<T> *der;
+    char bal;
+    T dato;
+
+    Nodo(const T &ele): izq(0), der(0), bal(0), dato(ele){}
+    Nodo(const Nodo<T> *orig): izq(0), der(0), bal(orig->bal), dato(orig->dato){}
+};
+
+template < typename T >
+class AVL {
+
+private:
+
+    Nodo < T >* raiz;
+
+    void recorreYCopia ( Nodo < T >*& pn, Nodo < T >*& po );
+
+
+public:
+
+    AVL ();
+    AVL ( const AVL < T >& orig);
+};
+
+
+//Constructor por defecto
+template < typename T >
+AVL < T >::AVL () : raiz ( 0 ) {}
+
+//Constructor copia
+template < typename T >
+AVL < T >::AVL ( const AVL<T>& orig ) {
+
+    this->recorreYCopia( this->raiz, orig.raiz );
+}
+
+template < typename T >
+void AVL < T >::recorreYCopia ( Nodo < T >*& pn, Nodo < T >*& po ) {
+
+    if ( po ) {
+
+        pn = new Nodo < T > ( po );
+
+        recorreYCopia( pn->izq, po->izq );
+        recorreYCopia( pn->der, po->der );
+
+    } else
+
+        pn = 0;
+}
+
+
+#endif //PR5_EEDD_AVL_H
